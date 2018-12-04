76e5577e63dbf2017a143ef4533eca3afbb46402
Hannah Beers <hannahlee0902@gmail.com>
2018-12-03 18:59:04 -0600

    title screen edits
    added elements and functionality to the title screen

---
+++ Ugly Ugly/Assets/Scripts/TutorialButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialButton : MonoBehaviour
+{
+
+    public void tutorialButton()
+    {
+
+        SceneManager.LoadScene("Tutorial");
+
+    }
+}
