576da4a51d8527c26f873090f701b3033f703884
Juliana <gessnerjules@gmail.com>
2018-12-09 01:50:55 -0600

    12-8/9-2018
    UPDATED TO LATEST VERSION OF UNITY
    
    added post processing stack v2, added vignette effect and chromatic abberation (and others but they're disabled atm), adjusted speed of walk animation for ugly. started NPC dialogue boxes and whatnot

---
+++ Ugly Ugly/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager dMan;
+
+	// Use this for initialization
+	void Start () {
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Ugly")
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                dMan.ShowBox(dialogue);
+            }
+        }
+    }
+}
